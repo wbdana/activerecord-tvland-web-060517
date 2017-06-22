c8d727bb7a979df6e7785fdc01437870eeb63f92
yfern328 <yfernan2@u.rochester.edu>
2017-06-22 18:48:01 -0400

    setup kind of complete?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 7) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -18,7 +18,10 @@ ActiveRecord::Schema.define(version: 5) do
   end
 
   create_table "characters", force: :cascade do |t|
-    t.string "name"
+    t.integer "actor_id"
+    t.integer "show_id"
+    t.string  "name"
+    t.string  "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
@@ -28,7 +31,10 @@ ActiveRecord::Schema.define(version: 5) do
 
   create_table "shows", force: :cascade do |t|
     t.string  "name"
+    t.string  "genre"
     t.integer "network_id"
+    t.text    "day"
+    t.text    "season"
   end
 
 end
